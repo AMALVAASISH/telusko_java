0e12afd82b99415de1d9ad98b8be384b697ea36f
AMALVAASISH <amalvaasish2000@gmail.com>
2025-04-03 20:40:12 +0530

    final completed

--- core_java/finally_key/Main.java
+++ core_java/finally_key/Main.java
@@ -2,10 +2,50 @@ package core_java.finally_key;
 
 // final can be used with variable,method and class
 
-public class Main {
-    public static void main(String[] args) {
-        
+// public class Main {
+//     public static void main(String[] args) {
+//         final int num = 9;
+//         // num = 3;// this cannot be done bcoz the variable is 
+//         // a const via final keyword
+//         System.out.println(num);
+//     }
+// }
+
+
+final class Calc{
+    public final void show(){
+        System.out.println("in calc show");
+    }
+
+    public void add(int a , int b){
+        System.out.println(a+b);
     }
 }
 
-// 7.17.08 here
+class AdvCalc extends Calc{
+    // if final is used with class, it cannot be inherited
+    
+}
+
+class CalcCalc{
+    public final void show(){
+
+    }
+}
+
+class SciCalc extends CalcCalc{
+    public void show(){
+        // the method over riding cannot take place
+        // bcoz the method is given final
+
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+        Calc obj = new Calc();
+        obj.show();
+        obj.add(1, 2);
+    }
+}
+
